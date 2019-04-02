3f72ac71707b19ad013073c0ecda9b020bd21f81
jdcardenas10 <jd.cardenas10@uniandes.edu.co>
2019-04-02 11:23:33 -0500

    Y esta

--- src/components/job.js
+++ src/components/job.js
@@ -1,16 +1,40 @@
 import React from 'react';
+import {FormattedDate} from 'react-intl';
+import {FormattedRelative} from 'react-intl';
+import {FormattedPlural} from 'react-intl';
+import {FormattedNumber} from 'react-intl';
+import {FormattedMessage} from 'react-intl';
 
 export default class Job extends React.Component {
 
   	render() {
+      let msg1=<FormattedMessage id="Million"/>;
+      let msg2=<FormattedMessage id="Millions"/>;
   		return (
   			<tr>
   				<th scope="row">{this.props.offer.id}</th>
-      			<td>{this.props.offer.name}</td>
-  				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
+      			<td><FormattedMessage id={this.props.offer.name}/></td>
+  				  <td>{this.props.offer.company}</td>
+      			<td>
+              {this.props.offer.salary}&emsp;
+              <FormattedPlural
+                value={this.props.offer.salary}
+                one={msg1}
+                other={msg2} 
+              />
+            </td>
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+      			<td>
+              <FormattedDate
+                value={new Date(this.props.offer.date)}
+                year='numeric'
+                month='long'
+                day='numeric'
+                weekday='long'
+              />&emsp;
+              (<FormattedRelative value={new Date(this.props.offer.date)}/>)
+            </td>
+            <td><FormattedNumber value={this.props.offer.views}/></td>
   			</tr>
   		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import {IntlProvider, addLocaleData} from 'react-intl';
+import esLocaleData from 'react-intl/locale-data/es';
+import localeEnMessages from "./locales/en";
+import localeEsMessages from "./locales/es";
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+addLocaleData(esLocaleData);
+
+function lenguaSelector(){
+   if (window.navigator.language.startsWith("es")) {
+        return (localeEsMessages);
+   }else{
+        return localeEnMessages;
+   }
+
+}
+
+ReactDOM.render(
+	<IntlProvider locale={window.navigator.language} messages= {lenguaSelector()}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
+
